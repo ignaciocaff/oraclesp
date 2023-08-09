51ab6b7dbcc62ce092f6e8cac8b9901e3a4d4a7e
jigcaffaratti@gmail.com <jigcaffaratti@gmail.com>
2023-08-09 20:31:53 -0300

    close rows

--- execution.go
+++ execution.go
@@ -2,11 +2,12 @@ package oraclesp
 
 import (
 	"github.com/ignaciocaff/oraclesp/internal/core"
+	"github.com/jmoiron/sqlx"
 )
 
 // ExecuteStore executes a stored procedure and maps the results to the provided results parameter.
 // It takes a database connection, context, stored procedure name, results interface{}, and optional arguments.
-func Execute(procedureName string, result interface{}, args ...interface{}) error {
-	core.ExecuteStoreProcedure(core.GetDB(), procedureName, result, args...)
+func Execute(db *sqlx.DB, procedureName string, result interface{}, args ...interface{}) error {
+	core.ExecuteStoreProcedure(db, procedureName, result, args...)
 	return nil
 }
