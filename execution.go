d3e42d7723122d3fc112de3d0173fd84a778aea0
jigcaffaratti@gmail.com <jigcaffaratti@gmail.com>
2023-08-30 09:14:07 -0300

    adding errors

--- execution.go
+++ execution.go
@@ -7,6 +7,9 @@ import (
 // ExecuteStore executes a stored procedure and maps the results to the provided results parameter.
 // It takes a database connection, context, stored procedure name, results interface{}, and optional arguments.
 func Execute(procedureName string, result interface{}, args ...interface{}) error {
-	core.ExecuteStoreProcedure(core.GetDB(), core.GetContext(), procedureName, result, args...)
+	err := core.ExecuteStoreProcedure(core.GetDB(), core.GetContext(), procedureName, result, args...)
+	if err != nil {
+		return err
+	}
 	return nil
 }
